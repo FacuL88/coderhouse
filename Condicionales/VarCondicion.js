d143dd447508a82e3c3c951a285140b87cb118b6
FacundoL <laugledev@gmail.com>
2022-06-07 11:51:41 -0300

    initial commit

---
+++ Condicionales/VarCondicion.js
@@ -0,0 +1,63 @@
+dineroFacu = prompt("¿Cuanto dinero tenes facu?");
+dineroDany = prompt("¿Cuanto dinero tenes dany?");
+dineroClau = prompt("¿Cuanto dinero tienes clau?");
+
+if (dineroFacu >= 1 && dineroFacu <= 3) {
+    alert("Comprate helado de limon")
+}
+
+else if (dineroFacu >= 3 && dineroFacu <= 6) {
+    alert("Comprate helado de dulce de leche")
+}
+
+else if (dineroFacu >= 6 && dineroFacu <= 9) {
+    alert("Comprate helado de chocolate")
+}
+
+else if (dineroFacu >= 9 && dineroFacu <= 12) {
+    alert("Comprate helado de tramontana")
+}
+
+else {
+    alert("no te alcanza para nada")
+}
+
+if (dineroDany >= 1 && dineroDany <= 3) {
+    alert("Comprate helado de limon")
+}
+
+else if (dineroDany >= 3 && dineroDany <= 6) {
+    alert("Comprate helado de dulce de leche")
+}
+
+else if (dineroDany >= 6 && dineroDany <= 9) {
+    alert("Comprate helado de chocolate")
+}
+
+else if (dineroDany >= 9 && dineroDany <= 12) {
+    alert("Comprate helado de tramontana")
+}
+
+else {
+    alert("No te alcanza")
+}
+
+if (dineroClau >= 1 && dineroClau <= 3) {
+    alert("Comprate helado de limon")
+}
+
+else if (dineroClau >= 3 && dineroClau <= 6) {
+    alert("Comprate helado de dulce de leche")
+}
+
+else if (dineroClau >= 6 && dineroClau <= 9) {
+    alert("Comprate helado de chocolate")
+}
+
+else if (dineroClau >= 9 && dineroClau <= 12) {
+    alert("Comprate helado de tramontana")
+}
+
+else {
+    alert("No te alcanza")
+}
