ce5b284361a20674e908edd2307f7abb62a0476b
FacundoL <laugledev@gmail.com>
2022-06-13 22:20:17 -0300

    modificacion

---
+++ Arrays/ABF.js
@@ -0,0 +1,24 @@
+/* let pc = {
+    nombre: "Facu",
+    procesador: "Intel dual core",
+    ram: "16GB",
+    espacio: "1TB"
+};
+
+let nombre = pc["nombre"];
+let procesador = pc["procesador"];
+let ram = pc["ram"];
+let espacio = pc["espacio"];
+
+
+frase = `el nombre de mi p c es: <b>${nombre}</b><br>
+         el procesador es: <b>${procesador}</b><br>
+         la memoria ram es: <b>${ram}</b><br>
+         el espacio en disco es: <b>${espacio}</b><br>
+         `
+
+         document.write(frase); */
+       
+let frutas = ["banana", "manzana", "pera", 5, 9, "pedro"];
+
+document.write(frutas[0]);
---
+++ clases-coder/coder.js
@@ -0,0 +1,42 @@
+/* for */
+
+/* for(let i = 0; i < 10; i++){
+    console.log("hola a todos", i);
+} */
+
+/* While */
+
+/* let nombre = prompt("ingrese un nombre");
+let limite = 0;
+
+while (nombre != "Facu") {
+    alert("nombre incorrecto");
+
+    if (limite === 3) {
+        break;
+    }
+
+    limite++;
+
+    nombre = prompt("ingrese el nombre correcto");
+}
+
+if (limite === 3) {
+    alert("estas bloqueado");
+}else {
+    alert("hola Facu");
+} */
+
+/* switch */
+
+let edad = 18;
+
+if (edad === 10) {
+    console.log("no puede entrar a la fiesta");
+}else if (edad === 15) {
+    console.log("puede entrar a la fiesta con un adulto");
+}else if (edad === 18) {
+    console.log("puede entrar con un amigo"); 
+}else {
+    console.log("puede entrar a la fiesta solo");
+}
