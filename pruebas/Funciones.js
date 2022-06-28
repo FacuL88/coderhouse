56a5b5fbcf78db03a93b2640e6e4941db20c7448
FacundoL <laugledev@gmail.com>
2022-06-28 16:32:42 -0300

    pruebas

---
+++ pruebas/Funciones.js
@@ -0,0 +1,50 @@
+/* const nombres = ["ariel", "facundo", "daniel", "claudia"];
+
+nombres.forEach(elemnto => {
+    alert(elemnto);
+}) */
+
+/* const productos = [
+    {nombre: "zapatos", precio: 1000},
+    {nombre: "alpargatas", precio: 500},
+    {nombre:"medias", precio: 250},
+];
+
+let busqueda = prompt("Ingrese el articulo para saber su precio")
+
+let producto = productos.find(elemento => elemento.nombre === busqueda);
+
+alert(`El precio de ${busqueda} es de ${producto.precio}`); */
+
+
+/* const productos = [
+    {nombre: "pc intel", precio: 10000},
+    {nombre: "pc amd", precio: 5800},
+    {nombre: "notebook", precio: 20000},
+];
+
+let filtrado = productos.filter(elemento => elemento.precio < 9000);
+
+console.log(filtrado);
+ */
+
+/* const productos = [
+    {nombre: "pc intel", precio: 48500},
+    {nombre: "pc amd", precio: 35500},
+    {nombre: "notebook", precio: 20000},
+];
+
+let valor = productos.some(elemento => elemento.precio === 700);
+
+console.log(valor); */
+
+const productos = [
+    {nombre: "pc intel", precio: 48500},
+    {nombre: "pc amd", precio: 35500},
+    {nombre: "notebook", precio: 20000},
+];
+
+let precios = productos.map(elemento => elemento.precio);
+let nombres = productos.map(elemento => elemento.nombre);
+
+console.log(nombres, precios);
